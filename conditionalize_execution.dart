22b35e3b2892b23ba77082065838e2fd607836b9
Mahdi Baha <mr.baahaa@gmail.com>
2021-02-09 06:25:18 -0800

    khelekes updated

---
+++ conditionalize_execution.dart
@@ -0,0 +1,14 @@
+void main() {
+  bool condition = true;
+  var kes = 0;
+  if (condition) {
+    kes += 1;
+  }
+  switch (condition) {
+    case false:
+      print('kes');
+      break;
+    case true:
+      print('gona dare khelekes !!!');
+  }
+}
