de00652978c5daabf90af8a10e67bdfb64c61c4b
Mahdi Baha <mr.baahaa@gmail.com>
2021-02-09 05:43:54 -0800

    this function.dart file is improved

--- functions.dart
+++ functions.dart
@@ -17,4 +17,16 @@ void main() {
   // return value from arrow functions
   sum2() => 1 + 2;
   print(sum2());
+
+  void something() {
+    print('hello world!');
+  }
+
+  something();
+
+  int sum4(int a, {int b: 10}) {
+    return a + b;
+  }
+
+  print(sum4(20));
 }
