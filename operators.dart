0f57fbf3dd678d51394a9a797000b57c5e681614
Mahdi Baha <mr.baahaa@gmail.com>
2021-02-09 05:51:35 -0800

    operator.dart file created.

---
+++ operators.dart
@@ -0,0 +1,14 @@
+void main() {
+  // + - * %
+  // / -> division result in double
+  // ~/ -> division result in int
+
+  // % -> remainder(باقی مانده)
+
+  // relational operators
+  // ==  !=  >  <  >=  <=
+
+  // Logical operators
+  // && -> and operator
+  // || -> or operator
+}
