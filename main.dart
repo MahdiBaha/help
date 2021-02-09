05d596746f4e421a0cb68de4e11134ccc7121137
Mahdi Baha <mr.baahaa@gmail.com>
2021-02-09 04:57:34 -0800

    add main file.

---
+++ main.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Variable Data Types
+  String name = 'Mahdi Baha';
+  int age = 20;
+  double float = 1.2;
+  bool isSmth = true;
+  List something = [];
+  var map = {'key': 'value', 'key2': 'value2'};
+
+  // dynamic
+  // With dynamic data type we can assign any type of value to any type of variable .
+  // So :
+  dynamic everything = 'everything';
+  // and we can convert it to another variable data type .
+  // So :
+  everything = 20;
+  print(everything);
+
+  // For defining constant we should use 'final' or 'const' keyword
+
+  final a = 'something';
+  const b = 'something';
+}
