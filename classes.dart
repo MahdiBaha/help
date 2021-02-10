e3893018071a17d1dbec78593ca400e7e04b3fef
Mahdi Baha <mr.baahaa@gmail.com>
2021-02-10 01:02:56 -0800

    classes examples added

--- classes.dart
+++ classes.dart
@@ -1,9 +1,29 @@
-void main() {}
+void main() {
+  User mahdi = new User('Mahdi Baha', 'develop3490', 'mr.baahaa@gmail.com');
+  mahdi.changeUsername('Javad Kalhor');
+  mahdi.changePassword('Maldari1379');
+  mahdi.changeEmail('Javad.gale_dar@gmail.com');
+  print(mahdi.username);
+  print(mahdi.password);
+  print(mahdi.email);
+}
 
-class People {
-  String name;
-  String lname;
-  int age;
-  bool isMarried;
-  
+class User {
+  String username;
+  String password;
+  String email;
+
+  User(this.username, this.password, this.email);
+
+  void changeUsername(String username) {
+    this.username = username;
+  }
+
+  void changePassword(String password) {
+    this.password = password;
+  }
+
+  void changeEmail(String email) {
+    this.email = email;
+  }
 }
